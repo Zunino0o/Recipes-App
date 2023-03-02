eae064692af7372bf1369ecbc5cd0a3d290bf758
Giovana Paludo <giovanacpaludo@gmail.com>
2023-03-02 16:17:24 -0300

    Adiciona rotas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/meals" component={ Meals } />
+          <Route exact path="/drinks" component={ Drinks } />
+          {/* <Route exact path="/meals/:id-da-receita" component={} /> */}
+          {/* <Route exact path="/drinks/:id-da-receita" component={} /> */}
+          {/* <Route exact path="/meals/:id-da-receita/in-progress" component={} /> */}
+          {/* <Route exact path="/drinks/:id-da-receita/in-progress" component={} /> */}
+          {/* <Route exact path="/profile" component={} /> */}
+          {/* <Route exact path="/done-recipes" component={} /> */}
+          {/* <Route exact path="/favorite-recipes" component={} /> */}
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
