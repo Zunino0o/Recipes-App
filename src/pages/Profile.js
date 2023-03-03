d3aed38a1c6a6a8834c964e44ce507d2b0c0af24
Lucas Zunino <zunino10@hotmail.com>
2023-03-03 15:05:39 -0300

    req57/renderiza componentes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,6 +2,29 @@ function Profile() {
   return (
     <div>
       <h1>Profile</h1>
+      <span data-testid="profile-email">E-MAIL</span>
+      <br />
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => console.log('Done Recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => console.log('Favorites Recipes') }
+      >
+        Favorites Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => console.log('Logout') }
+      >
+        Logout
+      </button>
     </div>
   );
 }
