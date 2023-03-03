5682e79e0633bce73d68ee4c3a93e0e477347930
Giovana Paludo <giovanacpaludo@gmail.com>
2023-03-03 10:43:22 -0300

    Requisitos 2, 3, 5 e parte dos reqs 4 e 6: elementos da tela de login e parte dos testes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginProvider from './context/LoginProvider';
 
 function App() {
   return (
     <div className="meals">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route exact path="/meals" component={ Meals } />
-          <Route exact path="/drinks" component={ Drinks } />
-          {/* <Route exact path="/meals/:id-da-receita" component={} /> */}
-          {/* <Route exact path="/drinks/:id-da-receita" component={} /> */}
-          {/* <Route exact path="/meals/:id-da-receita/in-progress" component={} /> */}
-          {/* <Route exact path="/drinks/:id-da-receita/in-progress" component={} /> */}
-          {/* <Route exact path="/profile" component={} /> */}
-          {/* <Route exact path="/done-recipes" component={} /> */}
-          {/* <Route exact path="/favorite-recipes" component={} /> */}
-        </Switch>
-      </BrowserRouter>
+      <LoginProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            {/* <Route exact path="/meals" component={ Meals } />
+          <Route exact path="/drinks" component={ Drinks } /> */}
+            {/* <Route exact path="/meals/:id-da-receita" component={} />
+          <Route exact path="/drinks/:id-da-receita" component={} />
+          <Route exact path="/meals/:id-da-receita/in-progress" component={} />
+          <Route exact path="/drinks/:id-da-receita/in-progress" component={} />
+          <Route exact path="/profile" component={} />
+          <Route exact path="/done-recipes" component={} />
+          <Route exact path="/favorite-recipes" component={} /> */}
+          </Switch>
+        </BrowserRouter>
+      </LoginProvider>
     </div>
   );
 }
