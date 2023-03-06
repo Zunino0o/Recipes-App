f52baf70c0e14588b6b541f90f950f9cba87ce7a
Lucas Zunino <zunino10@hotmail.com>
2023-03-06 17:18:52 -0300

    atualiza card de receitas favoritas

--- src/components/FavoriteCard.jsx
+++ src/components/FavoriteCard.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
-function FavoriteCard() {
+function FavoriteCard(recipe) {
+  const { category, name, image, index } = recipe;
   return (
     <section>
-      <img src="" alt="" />
-      <span>NOME</span>
-      <p>CATEGORIA</p>
-      <button>SHARE</button>
-      <button>FAV</button>
+      <img
+        src={ image }
+        alt={ name }
+        data-testid={ `${index}-horizontal-image` }
+      />
+      <span data-testid={ `${index}-horizontal-name` }>{ name }</span>
+      <p data-testid={ `${index}-horizontal-top-text` }>{ category }</p>
+      <button
+        type="button"
+        data-testid={ `${index}-horizontal-share-btn` }
+      >
+        <img src={ shareIcon } alt="compartilhar" />
+      </button>
+      <button
+        type="button"
+        data-testid={ `${index}-horizontal-favorite-btn` }
+      >
+        <img src={ blackHeartIcon } alt="desfavoritar" />
+      </button>
     </section>
   );
 }
