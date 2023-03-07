ce3e33b842c75326cb4f941b8b5e1df94ae7b678
Lucas Zunino <zunino10@hotmail.com>
2023-03-06 22:46:10 -0300

    conecta informacao vindo do LocalStorage com o componente

--- src/components/FavoriteCard.jsx
+++ src/components/FavoriteCard.jsx
@@ -2,30 +2,42 @@ import React from 'react';
 import shareIcon from '../images/shareIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 
-function FavoriteCard(recipe) {
-  const { category, name, image, index } = recipe;
+function FavoriteCard({ recipe, index }) {
+  const { id, type, nationality, category, alcoholicOrNot, name, image } = recipe;
+  console.log(recipe, index);
   return (
-    <section>
+    <div id={ id }>
       <img
         src={ image }
         alt={ name }
         data-testid={ `${index}-horizontal-image` }
       />
       <span data-testid={ `${index}-horizontal-name` }>{ name }</span>
-      <p data-testid={ `${index}-horizontal-top-text` }>{ category }</p>
+      <p
+        data-testid={ `${index}-horizontal-top-text` }
+      >
+        {
+          type === 'drink'
+            ? `${alcoholicOrNot} - ${category}`
+            : `${nationality} - ${category}`
+        }
+      </p>
+
       <button
+        id="share-btn"
         type="button"
         data-testid={ `${index}-horizontal-share-btn` }
-      >
-        <img src={ shareIcon } alt="compartilhar" />
-      </button>
+        src={ shareIcon }
+      />
+
       <button
+        id="favorite-btn"
         type="button"
         data-testid={ `${index}-horizontal-favorite-btn` }
-      >
-        <img src={ blackHeartIcon } alt="desfavoritar" />
-      </button>
-    </section>
+        src={ blackHeartIcon }
+      />
+
+    </div>
   );
 }
 
--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,28 +1,49 @@
+import FavoriteCard from '../components/FavoriteCard';
 import Header from '../components/Header';
 
 function FavoriteRecipes() {
+  // const favoriteRecipes = [{
+  //   id: '11566',
+  //   type: 'drink',
+  //   nationality: null,
+  //   category: 'Ordinary Drink',
+  //   alcoholicOrNot: 'Alcoholic',
+  //   name: 'Jewel Of The Nile',
+  //   image: 'https://www.thecocktaildb.com/images/media/drink/hx4nrb1504884947.jpg',
+  // },
+  // {
+  //   id: '52886',
+  //   type: 'meal',
+  //   nationality: 'British',
+  //   category: 'Dessert',
+  //   alcoholicOrNot: null,
+  //   name: 'Spotted Dick',
+  //   image: 'https://www.themealdb.com/images/media/meals/xqvyqr1511638875.jpg',
+  // }];
+
+  const localStorageData = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  const favoriteRecipes = localStorageData || [];
+  // console.log(favoriteRecipes);
+
   return (
     <section>
       <Header pageTitle="FavoriteRecipes" />
 
-      <button
-        type="button"
-        data-testid="filter-by-all-btn"
-      >
+      <button type="button" data-testid="filter-by-all-btn">
         All
       </button>
-      <button
-        type="button"
-        data-testid="filter-by-meal-btn"
-      >
+      <button type="button" data-testid="filter-by-meal-btn">
         Meals
       </button>
-      <button
-        type="button"
-        data-testid="filter-by-drink-btn"
-      >
+      <button type="button" data-testid="filter-by-drink-btn">
         Drinks
       </button>
+
+      <section>
+        {favoriteRecipes.map((rec, index) => (
+          <FavoriteCard key={ index } index={ index } recipe={ rec } />
+        ))}
+      </section>
     </section>
   );
 }
