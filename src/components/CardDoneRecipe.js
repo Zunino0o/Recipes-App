5537cd876a545397902eab239b353c82771fa73f
Rodrigo Almino <rdg.almino@gmail.com>
2023-03-09 14:04:54 -0300

    Finalização da pagina done recipes

--- src/components/CardDoneRecipe.js
+++ src/components/CardDoneRecipe.js
@@ -4,42 +4,24 @@ import ShareButton from './ShareButton';
 function CardDoneRecipe({ recipe, index }) {
   const { type } = recipe;
   const url = window.location.href;
+  const editURL = url.replace('/done-recipes', `/${recipe.type}s/${recipe.id}`);
 
-  if (type === 'meal') {
-    return (
-      <div>
+  return (
+    <div className="cardDoneRecipe">
+      <a href={ editURL }>
         <img
           src={ recipe.image }
           alt={ recipe.name }
           data-testid={ `${index}-horizontal-image` }
+          className="horizontal-image"
         />
         <h1 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h1>
-        <p data-testid={ `${index}-horizontal-top-text` }>
-          {`${recipe.nationality} - ${recipe.category}`}
-        </p>
-        <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
-        {recipe.tags.map((tag) => (
-          <p data-testid={ `${index}-${tag}-horizontal-tag` } key={ index }>
-            {tag}
-          </p>
-        ))}
-        <ShareButton
-          url={ url.replace('/done-recipes', `/${recipe.type}s/${recipe.id}`) }
-          dataTestid={ `${index}-horizontal-share-btn` }
-        />
-      </div>
-    );
-  }
-  return (
-    <div>
-      <img
-        src={ recipe.image }
-        alt={ recipe.name }
-        data-testid={ `${index}-horizontal-image` }
-      />
-      <h1 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h1>
+      </a>
+
       <p data-testid={ `${index}-horizontal-top-text` }>
-        {recipe.alcoholicOrNot}
+        {type === 'meal'
+          ? `${recipe.nationality} - ${recipe.category}`
+          : recipe.alcoholicOrNot}
       </p>
       <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
       {recipe.tags.map((tag) => (
@@ -47,17 +29,14 @@ function CardDoneRecipe({ recipe, index }) {
           {tag}
         </p>
       ))}
-      <ShareButton
-        url={ url.replace('/done-recipes', `/${recipe.type}s/${recipe.id}`) }
-        dataTestid={ `${index}-horizontal-share-btn` }
-      />
+      <ShareButton url={ editURL } dataTestid={ `${index}-horizontal-share-btn` } />
     </div>
   );
 }
 
 CardDoneRecipe.propTypes = {
   recipe: PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
@@ -67,7 +46,7 @@ CardDoneRecipe.propTypes = {
     alcoholicOrNot: PropTypes.string.isRequired,
     tags: PropTypes.arrayOf(PropTypes.string).isRequired,
   }).isRequired,
-  index: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default CardDoneRecipe;
--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,23 +1,46 @@
+import { useState } from 'react';
 import CardDoneRecipe from '../components/CardDoneRecipe';
 import Header from '../components/Header';
+import '../styles/DoneRecipes.css';
 
 function DoneRecipes() {
   const recipes = JSON.parse(localStorage.getItem('doneRecipes'));
+  const [filteredRecipes, setFilteredRecipes] = useState(recipes);
+
+  function applyFilter(filter) {
+    if (filter) {
+      setFilteredRecipes(recipes.filter((recipe) => recipe.type === filter));
+    } else {
+      setFilteredRecipes(recipes);
+    }
+  }
 
   return (
     <section>
       <Header pageTitle="Done Recipes" />
       <div>
-        <button type="button" data-testid="filter-by-all-btn">
-          Todos
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => applyFilter('') }
+        >
+          All
         </button>
-        <button type="button" data-testid="filter-by-meal-btn">
+        <button
+          type="button"
+          data-testid="filter-by-meal-btn"
+          onClick={ () => applyFilter('meal') }
+        >
           Meals
         </button>
-        <button type="button" data-testid="filter-by-drink-btn">
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => applyFilter('drink') }
+        >
           Drinks
         </button>
-        {recipes.map((recipe, index) => (
+        {filteredRecipes.map((recipe, index) => (
           <div key={ index }>
             <CardDoneRecipe recipe={ recipe } index={ index } />
           </div>
